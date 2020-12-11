4f0c6cdee04616c91db553570d086218b728fc38
Zhang, Shuning <sz31@illinois.edu>
2020-12-11 07:03:16 -0600

    Update GetDistance.cpp

--- Code/GetDistance.cpp
+++ Code/GetDistance.cpp
@@ -1,8 +1,11 @@
 #include "GetDistance.h"
 #include <cmath>
 #include <math.h>
-
-
+/*
+* this is a function changing the latitude and longtitude in to radius
+* @param  d   longitude or latitude inputed
+* @return The changed longitude or latitude value
+*/
 const double EARTH_RADIUS = 6378.137;//地球半径,radius of the earth
 double rad(double d)
 {
@@ -13,6 +16,11 @@ double rad(double d)
 * take out both of its latitude and longitude,and first convert it using the above function
 * then by applying the equation we found online to calculate the linear distance between two point
 * finally we scale it and return the distance
+* @param  lat1    latitude of the source airport
+* @param  lng1    longitude of the source airport
+* @param  lat2    latitude of the destination airport
+* @param  lng2    longitude of the destination airport
+* @return s       the distance between two points
 */
 double GetDistance(double lat1, double lng1, double lat2, double lng2)
 {
