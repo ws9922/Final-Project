f0d2e943f5b46d9e04d5bfc9d78d47cb9ae9705c
Zhang, Shuning <sz31@illinois.edu>
2020-12-11 06:40:04 -0600

    Update GetDistance.h

--- Code/GetDistance.h
+++ Code/GetDistance.h
@@ -2,6 +2,13 @@
 #include <cmath>
 #include <math.h>
 
+/*
+* this is a function calculating the distance between two airports,
+* take both of its latitude and longitude,and first convert it using the function rad
+* then by applying the equation we found online to calculate the linear distance between two point
+* finally we scale it and return the distance
+*/
+
 double rad(double d);
   
 double GetDistance(double lat1, double lng1, double lat2, double lng2);
